982f5c80c0368227af06dd99fd0ebb6b720bd398
Kaushal <kaushal14707@gmail.com>
2024-08-02 22:36:41 +0530

    completed everything
    small delete option bug

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const listContainer = document.getElementById("list-container");
 function addTask()
 {
     /*this function will add task */
-    if(inputBox.value === '')
+    if(inputBox.value ==="")
     {
         alert("You Must Write Something!");
     }
@@ -16,8 +16,10 @@ function addTask()
         let span = document.createElement("span");
         span.innerHTML = "<img src=\'./images/delete.png\'>";
         li.appendChild(span);
+        saveData(); 
     }
-    inputBox.value = " " ; 
+    inputBox.value = "" ; 
+    saveData(); 
 }
 
 listContainer.addEventListener("click",function(e)
@@ -25,10 +27,53 @@ listContainer.addEventListener("click",function(e)
     if(e.target.tagName === "LI")
     {
         e.target.classList.toggle("checked");
+        saveData(); 
     }
     else if(e.target.tagName === "SPAN")
     {
         e.target.parentElement.remove();
+        saveData(); 
     }
 }, false
 )
+
+inputBox.addEventListener("keypress",function(event)
+{
+    if(event.key === "Enter")
+    {
+        if(inputBox.value ==="")
+            {
+                alert("You Must Write Something!");
+            }
+            else
+            {
+                let li = document.createElement("li");
+                li.innerHTML = inputBox.value;
+                listContainer.appendChild(li);
+                let span = document.createElement("span");
+                span.innerHTML = "<img src=\'./images/delete.png\'>";
+                li.appendChild(span);
+                saveData(); 
+            }
+            inputBox.value = "" ;   
+            saveData();  
+    }        
+}
+)
+
+function show()
+{
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "flex";
+}
+
+function saveData()
+{
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask()
+{
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
